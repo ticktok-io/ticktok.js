7f8aaf6947fa3ea6b47504ef3f93ba29d432d053
Eli Segal <eli.segal@gmail.com>
2019-03-19 21:09:40 +0200

    [#157095526] Walking skeleton
    * first passing test
    * Added publishing to CI
    * deploy only from master

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'mocha': true
+  },
+  'extends': 'standard',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'space-before-function-paren': ['error', 'never']
+  }
+}
