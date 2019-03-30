49381ba63bbeae5d0fb6ee53d9837ae6ff4b7848
Eli Segal <eli.segal@gmail.com>
2019-03-30 14:49:28 +0300

    removed sanity tests

--- sanity/tests/simple-interval.js
+++ sanity/tests/simple-interval.js
@@ -6,15 +6,23 @@ const token = process.env.TICKTOK_TOKEN || 'ticktok-zY3wpR'
 describe('ticktok.io', () => {
   it('simple-interval', () => {
     let ticksReceived = 0
-    return new Promise((resolve, reject) => {
+    return new Promise(async(resolve, reject) => {
       setTimeout(() => reject(new Error('Failed to get enough ticks')), 4 * 1000)
-      ticktok(domain, token)
-        .schedule(clock.named('simple-interval').on('every.10.seconds').invoke(() => {
+      try {
+        schedule(() => {
           ticksReceived++
           if (ticksReceived === 3) {
             resolve()
           }
-        }))
+        })
+      } catch (err) {
+        reject(new Error('Failed to schedule a simple interval'))
+      }
     })
   })
+
+  const schedule = (callback) => {
+    return ticktok(domain, token)
+      .schedule(clock.named('simple-interval').on('every.10.seconds').invoke(callback))
+  }
 })
