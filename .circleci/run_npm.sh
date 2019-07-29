61e3e4f67546530c0884440484057c0907336e2f
Eli Segal <eli.segal@gmail.com>
2019-07-29 15:50:58 +0300

    wip

--- .circleci/run_npm.sh
+++ .circleci/run_npm.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-export RABBIT_URI=amqp://rabbit
-docker run --net=ci-network -v `pwd`:/opt/ci -w /opt/ci node:8-alpine npm "$@"
+docker run -e RABBIT_URI=amqp://rabbit --net=ci-network -v `pwd`:/opt/ci -w /opt/ci node:8-alpine npm "$@"
