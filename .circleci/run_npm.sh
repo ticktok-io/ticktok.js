094a47ec13aeac6dd1ce343cca211b28c7cc44f8
Eli Segal <eli.segal@gmail.com>
2019-07-29 14:33:46 +0300

    wip

--- .circleci/run_npm.sh
+++ .circleci/run_npm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --network=ci-network -v `pwd`:/opt/ci -w /opt/ci node:8-alpine npm "$@"
+docker run --net=ci-network --add-host=localhost:rabbit -v `pwd`:/opt/ci -w /opt/ci node:8-alpine npm "$@"
